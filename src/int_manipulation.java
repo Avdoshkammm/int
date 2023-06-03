3366e39a4df71e37022005f65d47a26988d1ec12
AvdoshkaMMM <AvdoshkaMMM@MSI>
2023-06-04 01:12:49 +0300

    Initial commit

---
+++ src/int_manipulation.java
@@ -0,0 +1,10 @@
+public class int_manipulation {
+    public static void main(String[] args) {
+        int a, b, c;
+
+        a = 5;
+        b = 5;
+        c = a % b;
+        System.out.println(c);
+    }
+}
